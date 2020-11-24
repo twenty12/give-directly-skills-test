5256e32721b1c92ebc9bbecab245517070259889
daniel.gladstone <gladstone.d@gmail.com>
2020-11-24 12:28:51 -0500

    add seed commands

--- src/api/commands/seed_database.py
+++ src/api/commands/seed_database.py
@@ -1,2 +1,17 @@
-def seed_databse():
-    print('go')
+from flask_script import Command
+from api import db
+from api.models import Book
+
+
+class SeedDB(Command):
+
+    def run(self):
+        book_titles = [
+            'The Death and Life of Great American Cities',
+            'Salt, Fat, Acid, Heat',
+            'The New Jim Crow'
+        ]
+        for title in book_titles:
+            new_book = Book(title=title)
+            db.session.add(new_book)
+            db.session.commit()
--- src/manage.py
+++ src/manage.py
@@ -2,14 +2,14 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from api import app, db
-from api.commands.seed_database import seed_databse
+from api.commands.seed_database import SeedDB
 
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-manager.add_command('seed_databse', seed_databse)
+manager.add_command('seed_db', SeedDB())
 
 if __name__ == '__main__':
     manager.run()
